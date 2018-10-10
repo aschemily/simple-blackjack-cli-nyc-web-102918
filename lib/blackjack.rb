24f89b822c5f05453db39e7c7d96e36a89ad89bd
Emily Asch <e.asch@me.com>
2018-10-10 00:35:07 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -62,6 +62,10 @@ def runner
   # code runner here
   welcome 
   card_total = 0 
+  card_total = initial_round
+  until card_total > 21 
+  card_total = hit?(card_total)
+  card_total = display_card_total
   
 end
     
