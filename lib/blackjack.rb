fd8e776c90000ace71ff28c0712f5fc1a92bf806
Emily Asch <e.asch@me.com>
2018-10-10 00:47:23 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,7 +36,7 @@ def initial_round
  deal_one = deal_card
  deal_two = deal_card
  card_total = deal_one + deal_two
- puts display_card_total(card_total)
+ display_card_total(card_total)
 end
 
 def hit?(current_card_total)
